572c3b7dd2d0991c5342aa7946660c41e8d89595
Shantanu Singh <shsingh@broadinstitute.org>
2016-08-09 12:56:09 -0400

    Added join_metadata.R

---
+++ join_metadata.R
@@ -0,0 +1,63 @@
+library(dplyr)
+library(magrittr)
+library(readr)
+library(stringr)
+
+args = commandArgs(trailingOnly=TRUE)
+
+batch_id <- args[1] #"2016_04_01_a549_48hr_batch1"
+
+plate_id <- args[2] #"SQ00015116"
+
+metadata_dir <- paste("../..", "metadata", batch_id, sep = "/")
+
+backend_dir <- paste("../..", "metadata", batch_id, plate, sep = "/")
+
+profiles <- read_csv(paste(backend_dir, paste0(plate_id, ".csv"), sep = "/"))
+
+# read profiles
+cnames <- colnames(profiles)
+
+cnames %<>% str_replace_all("^Image_Metadata", "Metadata")
+
+names(profiles) <- cnames
+
+# read and join metadata map
+
+metadata_map <- read_csv(paste(metadata_dir, "barcode_platemap.csv", sep = "/"))
+
+assert "Assay_Plate_Barcode" %in% colnames(metadata_map)
+
+cnames <- colnames(metadata_map)
+
+cnames %<>% str_replace_all("^", "Metadata_")
+
+names(metadata_map) <- cnames
+
+profiles %<>% mutate(Metadata_Assay_Plate_Barcode = Metadata_Plate)
+
+profiles %<>% inner_join(metadata_map, by = c("Metadata_Assay_Plate_Barcode"))
+
+# read and join platemap
+
+cpd_map_name <- profiles %>% select(Metadata_Compound_Plate_Map_Name) %>% distinct() %>% extract2("Metadata_Compound_Plate_Map_Name")
+
+expect_equal(length(cpd_map_name), 1)
+
+platemap <- read_tsv(paste(metadata_dir, "platemap_cpd", paste0(cpd_map_name, ".txt"), sep = "/"))
+
+assert "well_position " %in% colnames(platemap)
+
+cnames <- colnames(platemap)
+
+cnames %<>% str_replace_all("^", "Metadata_")
+
+names(platemap) <- cnames
+
+profiles %<>% mutate(Metadata_well_position = Metadata_Well)
+
+profiles %<>% inner_join(platemap, by = c("Metadata_well_position"))
+
+profiles_with_metadata <- paste(backend_dir, paste0(plate_id, "_augmented.csv"), sep = "/"))
+
+profiles %>% write_csv(profiles_with_metadata)
