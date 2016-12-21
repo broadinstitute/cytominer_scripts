04a553cf78b6c4231ef5392cb1947e1e9e552f6f
Shantanu Singh <shsingh@broadinstitute.org>
2016-12-21 02:17:25 -0500

    new file

---
+++ collapse.R
@@ -0,0 +1,61 @@
+#!/usr/bin/env Rscript
+
+'collapse
+
+Usage:
+collapse.R -b <id> -m <id> -o <file> [-f <str>]
+
+Options:
+-h --help                         Show this screen.
+-b <id> --batch_id=<id>           Batch ID.
+-m <id> --plate_map_name=<id>     Plate map name.
+-o <file> --output=<file>         Output CSV file.
+-f <str> --suffix=<str>           Suffix to append to barcode to select a profile file [default: _normalized_variable_selected.csv]' -> doc
+
+suppressWarnings(suppressMessages(library(docopt)))
+
+suppressWarnings(suppressMessages(library(dplyr)))
+
+suppressWarnings(suppressMessages(library(magrittr)))
+
+opts <- docopt(doc)
+
+batch_id <- opts[["batch_id"]]
+
+output <- opts[["output"]]
+
+plate_map_name <- opts[["plate_map_name"]]
+
+suffix <- opts[["suffix"]]
+
+backend_dir <- paste("../..", "backend", batch_id, sep = "/")
+
+metadata_dir <- paste("../..", "metadata", batch_id, sep = "/")
+
+barcode_platemap <- suppressMessages(readr::read_csv(paste0(metadata_dir, "/barcode_platemap.csv")))
+
+filelist <- barcode_platemap %>%
+  filter(Plate_Map_Name == plate_map_name) %>%
+  mutate(filename = normalizePath(
+    paste0(
+      backend_dir,
+      "/",
+      Assay_Plate_Barcode,
+      "/",
+      Assay_Plate_Barcode,
+      suffix
+    )
+  ))
+
+df <- lapply(filelist$filename, 
+    function(filename) {
+        if (file.exists(filename)) {
+            suppressMessages(readr::read_csv(filename))
+
+        } else {
+            tibble::data_frame()
+
+        }
+    })
+
+
