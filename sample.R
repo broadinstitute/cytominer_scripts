8d434755ba1a98074b5c2ed6c04a6a97b08714c4
Shantanu Singh <shsingh@broadinstitute.org>
2017-01-31 16:51:55 -0500

    Update sample.R

--- sample.R
+++ sample.R
@@ -18,6 +18,8 @@ suppressWarnings(suppressMessages(library(dplyr)))
 
 suppressWarnings(suppressMessages(library(magrittr)))
 
+suppressWarnings(suppressMessages(library(readr)))
+
 opts <- docopt(doc)
 
 batch_id <- opts[["batch_id"]]
