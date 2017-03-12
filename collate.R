41ad72e3e239f7f522990432d39ab17338ccab1a
Shantanu Singh <shsingh@broadinstitute.org>
2017-03-12 09:08:09 -0400

    typo

--- collate.R
+++ collate.R
@@ -95,9 +95,9 @@ if (!file.exists(cache_backend_file) | overwrite_backend_cache) {
   # add a column `Image_Metadata_Plate` if specified
 
   if(!is.null(column_as_plate)) {
-    system("sqlite3 ALTER TABLE Image ADD COLUMN Image_Metadata_Plate TEXT;")
+    system("sqlite3", cache_backend_file, "'ALTER TABLE Image ADD COLUMN Image_Metadata_Plate TEXT;'")
 
-    system(paste("sqlite3 UPDATE image SET Image_Metadata_Plate =", column_as_plate, ";"))
+    system(paste("sqlite3", cache_backend_file, "'UPDATE image SET Image_Metadata_Plate =", column_as_plate, ";'"))
 
   }
 
